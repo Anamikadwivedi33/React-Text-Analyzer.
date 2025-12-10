4aea4a066780753280e501eeced90c5d3e4340ba
anamika <anamikadwivedi367@gmail.com>
2025-12-10 11:18:27 +0530

    Update Forms.js components

--- src/Components/Forms.js
+++ src/Components/Forms.js
@@ -18,6 +18,29 @@ const handleUpClicke= ()=>{
   let newText =text.toUpperCase();
   setText(newText)
 }
+
+ 
+
+
+
+const handleClearClicke= ()=>{
+
+ // console.log("upper case was clicked" + text)
+  let newText =" ";
+  setText(newText)
+}
+
+
+const handleLoClicke= ()=>{
+
+ // console.log("upper case was clicked" + text)
+  let newText =text.toLowerCase();
+  setText(newText)
+}
+
+
+
+
   const [text, setText] = useState('enter text here');
   
   return (
@@ -34,7 +57,10 @@ const handleUpClicke= ()=>{
 </div>
 
 
-<button type="button" className="btn btn-primary btn-lg" onClick={handleUpClicke}   >convert into uppercase</button>
+<button type="button" className="btn btn-primary btn-lg mx-2" onClick={handleUpClicke}   >convert into Uppercase</button>
+<button type="button" className="btn btn-primary btn-lg mx-1" onClick={handleLoClicke}   >convert into Lowercase</button>
+
+<button type="button" className="btn btn-primary btn-lg mx-2" onClick={handleClearClicke}   >Clear Text</button>
     </div>
 
 
